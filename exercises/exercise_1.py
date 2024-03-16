79584cc89613c4e769aa2abb3e573e05171fe0c9
Harvey-Kp1 <158475210+Harvey-Kp1@users.noreply.github.com>
2024-03-16 10:40:03 +0000

    Update exercise_1.py

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,5 +1,5 @@
-n = int(input("Enter the number of seconds you want the countdown to start from"))
+n = int(input("Enter the number of seconds you want the countdown to start from: "))
 while n>0:
   print(n)
-  n-1
+  n=n-1
 print("start!")
